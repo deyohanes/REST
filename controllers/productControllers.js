7792b5079e2cfd1953a92498aca849665ca4e732
yohanes derese <49651408+deyohanes@users.noreply.github.com>
2022-04-27 03:21:22 +0300

    some

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -27,13 +27,28 @@ const setProduct =asyncHAndler( async(req, res) => {
 //@route PUT/api/goals
 //@access private
 const updateProduct =asyncHAndler( async(req, res) => {
-  res.status(200).json({ message: `update dddd ${req.params.id}` });
+
+  const product = await Products.findById(req.params.id)
+  if(!product){
+    res.status(400)
+    throw new Error('Product Not Found') 
+  }
+  const updateProduct = await Products.findByIdAndUpdate(req.params.id,req.body,{
+    new : true
+  })
+  res.status(200).json(updateProduct);
 });
 //@desc get products
 //@route delete/api/goals
 //@access private
 const deleteProduct =asyncHAndler( async(req, res) => {
-  res.status(200).json({ message: `delete dddd ${req.params.id}` });
+  const product = await Products.findById(req.params.id)
+  if(!product){
+    res.status(400)
+    throw new Error('Product Not Found') 
+  }
+  await products.remove()
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
